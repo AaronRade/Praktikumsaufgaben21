34b02a2d89c63ac09b80df79a96e45b3123428cd
Tim Sippel <sippel@amicaldo.de>
2017-11-23 11:59:09 +0100

    add date class

--- src/aufgabe2/Date.java
+++ src/aufgabe2/Date.java
@@ -1,9 +1,29 @@
 package aufgabe2;
 
 public class Date {
+    private PointInTime begin;
+    private Period length;
+    private String label;
 
-    public Date (PointInTime begin, Period length){
-        
+    public Date (PointInTime begin, Period length, String label){
+        this.begin = begin;
+        this.length = length;
+        this.label = label;
 
+    }
+
+    public void set(PointInTime begin, Period length) {
+        this.begin = begin;
+        this.length = length;
+    }
+
+    @Override
+    public Date clone() {
+       return new Date(begin, length, label);
+    }
+
+    @Override
+    public String toString() {
+        return "Label: \t" + label + "\nBegin:\t" + (begin == null ? "Unknown" : begin) + "\nLength:\t" + (length == null ? "Unknown" : length) ;
     }
 }
--- src/aufgabe2/TestAllClasses.java
+++ src/aufgabe2/TestAllClasses.java
@@ -5,26 +5,39 @@ public class TestAllClasses {
     public static void main(String[] args) {
         // TODO: teste hier die einzelnen erstellten Klassen!
 
-        Period Period1 = new Period(2, 40);
+        Period period1 = new Period(2, 40);
         Period Period2 = new Period(1, 30);
 
         //expected results commented
 
-        System.out.println("Period1: "+Period1);
+        System.out.println("Period1: "+period1);
         // 2:40
         System.out.println("Period2: "+Period2);
         // 1:30
-        System.out.println("Period1 Minuten insgesamt: "+Period1.getMinutes());
+        System.out.println("Period1 Minuten insgesamt: "+period1.getMinutes());
         // 160
-        System.out.println("Period1 Stunden: "+Period1.getHours());
+        System.out.println("Period1 Stunden: "+period1.getHours());
         // 2
-        System.out.println("Period1 Minuten: "+Period1.getMinorMinutes());
+        System.out.println("Period1 Minuten: "+period1.getMinorMinutes());
         // 40
         System.out.println("Period2 cloned: "+Period2.clone());
         // 1:30
         System.out.println("Period2 change mit 10: "+Period2.change(10));
         // 1:40
-        System.out.println("Period1 change mit 70: "+Period1.change(70));
+        System.out.println("Period1 change mit 70: "+period1.change(70));
         // 3:50
+
+        Date date1 = new Date(null, period1, "Dies ist ein Test");
+
+        Date date2 = date1.clone();
+
+        System.out.println(date1);
+        System.out.println(date2);
+
+        date1.set( null, null );
+        System.out.println("-----------------------");
+
+        System.out.println(date1);
+        System.out.println(date2);
     }
 }
